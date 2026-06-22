84207d02451d09de9547b8148f20117bb02654bd
unknown <damaresgaia@gmail.com>
2026-06-21 21:04:04 -0300

    test: implementa testes unitarios e mocks para motor de rega e validacoes

--- app/src/test/java/mocks/EmissaoAlertasMock.java
+++ app/src/test/java/mocks/EmissaoAlertasMock.java
@@ -1,4 +1,20 @@
 package mocks;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EmissaoAlertasMock {
+    private final List<String> alertasSimulados = new ArrayList<>();
+
+    public void adicionarAlerta(String alerta) {
+        alertasSimulados.add(alerta);
+    }
+
+    public List<String> getAlertasSimulados() {
+        return alertasSimulados;
+    }
+
+    public boolean contemAlertaPara(String apelido) {
+        return alertasSimulados.stream().anyMatch(a -> a.contains(apelido));
+    }
 }
--- app/src/test/java/mocks/RepositorioDadosMock.java
+++ app/src/test/java/mocks/RepositorioDadosMock.java
@@ -1,4 +1,47 @@
 package mocks;
 
-public class RepositorioDadosMock {
+import model.Especie;
+import model.Planta;
+import repository.IDadosRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RepositorioDadosMock implements IDadosRepository {
+    private final List<Planta> plantas = new ArrayList<>();
+    private final List<Especie> especies = new ArrayList<>();
+    public boolean salvouDados = false;
+    public boolean carregouDados = false;
+
+    @Override
+    public void salvarPlanta(Planta planta) {
+        plantas.removeIf(p -> p.getApelido().equalsIgnoreCase(planta.getApelido()));
+        plantas.add(planta);
+    }
+
+    @Override
+    public List<Planta> obterTodasPlantas() {
+        return plantas;
+    }
+
+    @Override
+    public void salvarEspecie(Especie especie) {
+        especies.removeIf(e -> e.getNome().equalsIgnoreCase(especie.getNome()));
+        especies.add(especie);
+    }
+
+    @Override
+    public List<Especie> obterTodasEspecies() {
+        return especies;
+    }
+
+    @Override
+    public void salvarDados() {
+        salvouDados = true;
+    }
+
+    @Override
+    public void carregarDados() {
+        carregouDados = true;
+    }
 }
